9916bfd088ee5615e698630666690b770ddbb633
RajIshu <gokuishu57@gmail.com>
2022-07-16 23:01:53 +0530

    Added Coding Exercises

---
+++ Codes/MinAndMaxChallenge/src/MinAndMax.java
@@ -0,0 +1,19 @@
+/*
+Problem Description:
+
+-Read the numbers from the console entered by the user and print the minimum
+ and maximum number the user has entered.
+-Before the user enters the number, print the message "Enter number:"
+-If the user enters an invalid number, break out of the loop and print the
+ minimum and maximum number.
+
+Hint:
+-Use an endless while loop.
+
+Bonus:
+-Create a project with the name MinAndMaxInputChallenge.
+*/
+
+
+public class MinAndMax {
+}
