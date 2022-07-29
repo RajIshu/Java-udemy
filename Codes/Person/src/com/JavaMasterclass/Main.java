838ffeed30d44d5e5a5505f6c9d4c4d48e651318
RajIshu <gokuishu57@gmail.com>
2022-07-29 23:40:46 +0530

    Added Coding Exercises

--- Codes/Person/src/com/JavaMasterclass/Main.java
+++ Codes/Person/src/com/JavaMasterclass/Main.java
@@ -68,5 +68,23 @@ NOTE: Do not add the main method to the solution code.
 */
 
 
-public class Person {
+
+package com.JavaMasterclass;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setFirstName("");   // firstName is set to empty string
+        person.setLastName("");    // lastName is set to empty string
+        person.setAge(10);
+        System.out.println("fullName= " + person.getFullName());
+        System.out.println("teen= " + person.isTeen());
+        person.setFirstName("John");    // firstName is set to John
+        person.setAge(18);
+        System.out.println("fullName= " + person.getFullName());
+        System.out.println("teen= " + person.isTeen());
+        person.setLastName("Smith");    // lastName is set to Smith
+        System.out.println("fullName= " + person.getFullName());
+    }
 }
