15f4da71ae03f4d899b4373bdfc3dc80f4293373
RajIshu <gokuishu57@gmail.com>
2022-07-29 20:46:05 +0530

    Added Classes

---
+++ Codes/Classes/src/com/JavaMasterclass/Car.java
@@ -0,0 +1,4 @@
+package com.JavaMasterclass;
+
+public class Car {
+}
---
+++ Codes/Classes/src/com/JavaMasterclass/Main.java
@@ -0,0 +1,33 @@
+package com.JavaMasterclass;
+
+public class Main {
+
+    public static void main(String[] args) {
+	/* OBJECTS:
+	    - Every real world Object has two major characteristics:
+	        1. State
+	        2. Behavior
+
+	    - State is nothing but specification of the system
+	      Like, a computer's RAM, type of OS it is running, it's storage capacity, etc.
+
+	    - Behavior for computer would be booting up, shutting down, giving output, printing something,
+	      performing calculation, etc.
+
+	    Similar to this, Objects in OOP i.e., software objects also have STATE and BEHAVIOR.
+
+	    - State of the software objects are stored in fields known as VARIABLES.
+	    - Behaviors of these software objects are exposed through METHODS.
+
+
+	  CLASS:
+	    - Class is a template or a blueprint for creating Objects.
+
+	    - Benefits of Class:
+	        1. Basic or primitive data types are limited i.e., int, short, etc.
+	        2. Class give us a power to define our own data type called User-defined Data Type.
+	*/
+
+
+    }
+}
