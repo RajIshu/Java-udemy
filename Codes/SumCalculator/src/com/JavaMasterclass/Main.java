627c3f4d9dad0937ff0e5c04174be460f7f1a76d
RajIshu <gokuishu57@gmail.com>
2022-07-29 23:24:10 +0530

    Added Coding Exercises

---
+++ Codes/SumCalculator/src/com/JavaMasterclass/Main.java
@@ -0,0 +1,16 @@
+package com.JavaMasterclass;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SimpleCalculator calculator = new SimpleCalculator();
+        calculator.setFirstNumber(5.0);
+        calculator.setSecondNumber(4);
+        System.out.println("add= " + calculator.getAdditionResult());
+        System.out.println("subtract= " + calculator.getSubtractionResult());
+        calculator.setFirstNumber(5.25);
+        calculator.setSecondNumber(0);
+        System.out.println("multiply= " + calculator.getMultiplicationResult());
+        System.out.println("divide= " + calculator.getDivisionResult());
+    }
+}
---
+++ Codes/SumCalculator/src/com/JavaMasterclass/SimpleCalculator.java
@@ -0,0 +1,41 @@
+package com.JavaMasterclass;
+
+public class SimpleCalculator {
+    private double firstNumber;
+    private double secondNumber;
+
+    public double getFirstNumber() {
+        return firstNumber;
+    }
+
+    public double getSecondNumber() {
+        return secondNumber;
+    }
+
+    public void setFirstNumber(double firstNumber) {
+        this.firstNumber = firstNumber;
+    }
+
+    public void setSecondNumber(double secondNumber) {
+        this.secondNumber = secondNumber;
+    }
+
+    public double getAdditionResult(){
+        return firstNumber + secondNumber;
+    }
+
+    public double getSubtractionResult(){
+        return firstNumber - secondNumber;
+    }
+
+    public double getMultiplicationResult(){
+        return firstNumber * secondNumber;
+    }
+
+    public double getDivisionResult(){
+        if(secondNumber == 0){
+            return 0;
+        }
+        return firstNumber / secondNumber;
+    }
+}
