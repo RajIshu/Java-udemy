8751b429ff745ada10efc1e492aba485d2e50d21
RajIshu <gokuishu57@gmail.com>
2022-10-04 23:10:09 +0530

    update Carpet Cost Calculator

--- Codes/CarpetCostCalculator/src/CarpetCostCalc.java
+++ Codes/CarpetCostCalculator/src/CarpetCostCalc.java
@@ -80,4 +80,17 @@ NOTE: Do not add a main method to the solution code.
 
 
 public class CarpetCostCalc {
+
+    public static void main(String[] args) {
+
+        Carpet carpet = new Carpet(3.5);
+        Floor floor = new Floor(2.75, 4.0);
+        Calculator calculator = new Calculator(floor, carpet);
+        System.out.println("total= " + calculator.getTotalCost());
+        carpet = new Carpet(1.5);
+        floor = new Floor(5.4, 4.5);
+        calculator = new Calculator(floor, carpet);
+        System.out.println("total= " + calculator.getTotalCost());
+
+    }
 }
