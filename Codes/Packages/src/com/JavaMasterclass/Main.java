4ecc8ae1618375acf4244bf1b327fae3537218d7
RajIshu <gokuishu57@gmail.com>
2022-11-13 02:36:41 +0530

    add Packages

---
+++ Codes/Packages/src/com/JavaMasterclass/Main.java
@@ -0,0 +1,12 @@
+package com.JavaMasterclass;
+
+import javafx.scene.Node;
+import org.w3c.dom.Node;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        javafx.scene.Node = null;
+    }
+}
